ee8bce99747a6f6531b057cf586e7ff13503a62b
ujjwalhat <ujjwal31072002@gmail.com>
2024-11-05 13:30:41 +0530

    Api for update

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,9 +1,10 @@
+import { validateMyUserRequest } from '../middleware/validation';
 import express from "express"
 import {createCurrentUserr, updateCurrentUser} from "../contollers/MyUserController"
 import { jwtCheck, jwtParse } from "../middleware/auth";
 
 const router = express.Router();
 router.post('/',jwtCheck,createCurrentUserr)
-router.put("/",jwtCheck,jwtParse,updateCurrentUser)
+router.put("/",jwtCheck,jwtParse,validateMyUserRequest,updateCurrentUser)
 
 export default router
