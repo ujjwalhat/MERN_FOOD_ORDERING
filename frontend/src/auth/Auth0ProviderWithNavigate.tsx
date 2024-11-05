61dca9a05c7e15f46f075c127913b63c07fdce8d
ujjwalhat <ujjwal31072002@gmail.com>
2024-11-05 11:28:08 +0530

    Some other works

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,20 +1,24 @@
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { AppState, Auth0Provider, useAuth0, User } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { getAccessTokenSilently } = useAuth0();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const navigate = useNavigate();
 
   if (!domain || !clientId || !redirectUri) {
     throw new Error("Unable to initialise auth");
   }
-  const onRedirectCallBack = (appState?: AppState, user?: User)=>{
-    console.log("USER",user);
-
-  }
+  const onRedirectCallBack = async (appState?: AppState, user?: User) => {
+    const token = await getAccessTokenSilently();
+    console.log("token", token);
+    navigate("/auth-callback");
+  };
   return (
     <Auth0Provider
       domain={domain}
