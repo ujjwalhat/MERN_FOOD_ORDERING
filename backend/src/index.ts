6686a2e4ff13ad1ffb142b4356b92470017a7a4f
ujjwalhat <ujjwal31072002@gmail.com>
2024-11-06 23:51:08 +0530

    Simple Changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,23 @@
-import express, {Request,Response} from "express"
-import cors from "cors"
-import "dotenv/config" 
-import mongoose from "mongoose"
-import myUserRoute from "./routes/MyUserRoute"
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
-mongoose.connect(process.env.MONGODB_URL as string).then(()=>console.log("Connected to DB"))
+mongoose
+  .connect(process.env.MONGODB_URL as string)
+  .then(() => console.log("Connected to DB"));
 
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+app.get("/health", (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
 
-app.use("/api/my/user",myUserRoute)
+app.use("/api/my/user", myUserRoute);
 
-app.listen(8000,()=>{
-    console.log("SERVER STARTED AT : 8000")
-})
+app.listen(8000, () => {
+  console.log("SERVER STARTED AT : 8000");
+});
